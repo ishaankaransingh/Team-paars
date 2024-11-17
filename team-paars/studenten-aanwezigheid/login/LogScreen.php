4e9a2c9e0fa0cc21df7d0ff853685f8d707b4896
SnelleWerker <rashawnkun@gmail.com>
2024-11-17 19:19:07 -0300

    kijk
    kijk zelf

--- team-paars/studenten-aanwezigheid/login/LogScreen.php
+++ team-paars/studenten-aanwezigheid/login/LogScreen.php
@@ -15,7 +15,7 @@
             <p class="error"><?php echo $_GET['error']; ?></p>
         <?php } ?>
         
-        <label>User Name of Email</label>
+        <label>Username of Email</label>
         <input type="text" name="username" placeholder="Username of email" required><br>
 
         <label>Password</label>
