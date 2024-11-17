9642ea1e9293d68cf9d6f37e64489a9ce375b05f
ishaankaransingh <ishaan.karansingh@student.natin.sr>
2024-11-17 12:48:31 -0300

    Create LogScreen.php

---
+++ team-paars/studenten-aanwezigheid/login/LogScreen.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+</head>
+<body>
+    <form action="login.php" method="post">
+        
+    <div class="logo-container">
+        <img src="../CSS/fotos/natinlogo.png" alt="Natin Logo" style="width: 100%; max-width: 300px;"/> <!-- Adjust the path and size as needed -->
+    </div>
+
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+        
+        <label>User Name</label>
+        <input type="text" name="username" placeholder="User  Name" required><br>
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password" required><br>
+
+        <button type="submit">Log in</button>
+    </form>
+</body>
+</html>
